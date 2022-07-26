05b008dfe6c3eaf082e062ae5a7284595bb29352
Justice Johnson <79010796+justlyJustice@users.noreply.github.com>
2022-07-26 13:50:04 +0100

    Initial push to github

---
+++ src/components/Class.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Class extends Component {
+  render() {
+    return <h2>Hello World</h2>;
+  }
+}
+
+export default Class;
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Todo } from "../model";
+import { AiFillDelete, AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
+import "./styles.css";
+
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const SingleTodo: React.FC<Props> = ({ setTodos, todo, todos }) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  return (
+    <form className="todos__single">
+      <span className="todos__single--text">{todo.todo}</span>
+
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
